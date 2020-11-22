bef7f716718569babc126efd424cb5e6b3ef9840
revmat56 <62846638+revmat56@users.noreply.github.com>
2020-11-22 23:56:58 +0100

    Update transportujFlota.hpp

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,32 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
+    
+	if (towar != 0)
+	{
+		unsigned int przewieziony_towar=0;
+		unsigned int ilosc_zaglowcow=0;
+		
+		do
+		{
+			Stocznia stocznia{};
+			Statek* s1 = stocznia();
+			przewieziony_towar += s1->transportuj();
+			if (Zaglowiec* z = dynamic_cast<Zaglowiec*>(s1))
+			{
+				ilosc_zaglowcow += 1;
+			}
+			delete s1;
+		}
+		while(przewieziony_towar < towar);
+		
+		std::cout << "Funkcja stworzyla: " << ilosc_zaglowcow << " zaglowcow";
+        
+        return ilosc_zaglowcow;
+	}
+    else
+    {
+    	std::cout<< "Nie utworzono, zadnego statku - nie bylo towaru do przewiezienia" ;
+	}
     return 0;
 }
